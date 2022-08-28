cb4df0af0da4d8eda352698a753f60e4ee9b79b0
Eph <ephraim@modevx.com>
2022-08-28 16:46:02 -0500

    unbreak AlbersFromTopoJSON

--- src/components/maps/AlbersProjectedAsPath.jsx
+++ src/components/maps/AlbersProjectedAsPath.jsx
@@ -1,47 +1,40 @@
-import React from "react";
-import { ProjectionPath } from "../shapes";
-import { getGeoJsonFromTopoJson } from "../../utils/getGeoJsonFromTopoJson";
-
 // ----- FOLLOWED THIS VID TO GET WORKING PROTOTYPE ----------
 // -- https://www.youtube.com/watch?v=mzZ1fCXq-uo&t=615s
 // -- https://unpkg.com/world-atlas@2.0.2/countries-50m.json
 // ------------------------------------------------------------
+import * as d3 from "d3";
+import React from "react";
+import { feature } from "topojson-client";
+import AlbersTopoJSON from "../../geometry_files/topoJSON/albers_fips.json";
 
-export const AlbersProjectedAsPath = ({
-	topoJson,
-	geometryToUse,
-	projection,
-	children,
-}) => {
+export const AlbersProjectedAsPath = ({ children }) => {
 	const [geoJsonGeometry, setGeoJsonGeometry] = React.useState(null);
-
-	console.log(
-		">> geoJsonGeometry for GeoJsonSvgProjectionPath: \n",
-		geoJsonGeometry
-	);
-
-	const fromTopoJSON = getGeoJsonFromTopoJson(topoJson, geometryToUse);
+	const projection = d3.geoAlbers();
+	const createSVGPath = d3.geoPath(projection);
 
 	React.useEffect(() => {
-		if (fromTopoJSON) {
-			setGeoJsonGeometry(fromTopoJSON?.geoJSON);
-		}
-	}, [fromTopoJSON]);
+		const generateGeoJson = async () => {
+			const geoJSON = await feature(AlbersTopoJSON, "states");
+			setGeoJsonGeometry(geoJSON);
+		};
+
+		generateGeoJson();
+	}, []);
+
+	geoJsonGeometry && console.log(geoJsonGeometry);
 
 	return (
-		<svg
-			// width='975px'
-			// height='auto'
-			viewBox='0 0 975 610'
-			className='border-2 border-white'
-		>
-			{geoJsonGeometry && (
-				<ProjectionPath
-					geoJsonFeatures={geoJsonGeometry}
-					projection={projection}
-				/>
-			)}
-			{children}
+		<svg viewBox='0 0 975 610' className='border-2 border-white fill-blue-500'>
+			{geoJsonGeometry &&
+				geoJsonGeometry.features.map((feature) => {
+					return (
+						<path
+							key={feature.id}
+							d={createSVGPath(feature)}
+							className='stroke-white'
+						/>
+					);
+				})}
 		</svg>
 	);
 };
