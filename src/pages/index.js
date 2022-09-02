df90bac05f41e975d6e15f6b0ce2584cff0eec26
Eph <ephraim@modevx.com>
2022-09-02 06:31:08 -0500

    overlay spc convective outlook on Albers SVG map

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,29 +13,29 @@ import { AlbersConvective } from "../components/maps/AlbersConvective";
 export default function Home() {
 	return (
 		<Layout>
-			<div className='grid gap-16 sm:grid-cols-2 grid-flow-row place-items-center'>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<AlbersConvective />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<RVMHoverableStates />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<AlbersProjectedAsPath />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<RVMRotatedGradientCover />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<RVMRadialGradient />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<RVMOverlayShape />
-				</div>
-				<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
-					<AlbersCounties />
-				</div>
+			{/* <div className='grid gap-16 sm:grid-cols-2 grid-flow-row place-items-center'> */}
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<AlbersConvective />
 			</div>
+			{/* <div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<RVMHoverableStates />
+			</div>
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<AlbersProjectedAsPath />
+			</div>
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<RVMRotatedGradientCover />
+			</div>
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<RVMRadialGradient />
+			</div>
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<RVMOverlayShape />
+			</div>
+			<div className='bg-gradient-to-r from-zinc-900 to-zinc-600 p-6 rounded-lg shadow-black shadow-lg'>
+				<AlbersCounties />
+			</div> */}
+			{/* </div> */}
 		</Layout>
 	);
 }
