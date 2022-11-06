d2f1662b2b4d9a8c9273a154804dcf9e7ae4f84f
Ephraim <ephraim@modevx.com>
2022-11-06 03:12:56 -0600

    update branding title

--- src/components/Branding.jsx
+++ src/components/Branding.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 export const Branding = () => {
-	return (
-		<Link href='/'>
-			<a className='inline-block'>
-				<h1 className='font-bold text-lg text-white'>KARTELAND</h1>
-			</a>
-		</Link>
-	);
+  return (
+    <Link href="/">
+      <a className="inline-block">
+        <h1 className="font-bold text-lg text-white">SAVAGE MAPS</h1>
+      </a>
+    </Link>
+  );
 };
