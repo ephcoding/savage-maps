d8aceefecdb70ede94a02dc5dbcd41490d2d2e9f
Eph <ephraim@modevx.com>
2023-09-08 01:15:33 -0500

    add metadata

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
 	title: "Savage Maps",
 	description: "Interactive maps powered by D3.js",
+	openGraph: {
+		title: "Savage Maps",
+		siteName: "Savage Maps",
+		description: "Interactive maps powered by D3.js",
+		locale: "en_US",
+		type: "website",
+		url: "https://savagemaps.com",
+		images: [
+			{
+				url: "/public/savage-maps-preview.png",
+				width: 1600,
+				height: 900,
+				alt: "Savage Maps social preview image",
+			},
+		],
+	},
 };
 
 export default function RootLayout({ children }) {
