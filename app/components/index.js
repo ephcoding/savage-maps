cafac07e2061749f4983aa688a898fc382076a23
Eph <ephraim@modevx.com>
2023-09-08 02:35:58 -0500

    add BgMap to Homescreen

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,4 @@
+export * from "./BackgroundMap";
 export * from "./Footer";
 export * from "./Header";
 export * from "./SavageLogo";
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { BackgroundMap } from "./components";
+
+export default function Home() {
+	return (
+		<main className='flex-1'>
+			<BackgroundMap />
+		</main>
+	);
+}
