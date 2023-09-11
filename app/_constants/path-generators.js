76fa6e5d7a301e87f8a0e5a37cc63e980ddb8401
Ephraim <ephraim@modevx.com>
2023-09-11 18:56:10 -0500

    create D3 path generator constants

---
+++ app/_constants/path-generators.js
@@ -0,0 +1,97 @@
+import * as topojson from "topojson-client";
+import {
+  geoPath,
+  geoAzimuthalEqualArea,
+  geoAzimuthalEquidistant,
+  geoGnomonic,
+  geoOrthographic,
+  geoStereographic,
+  geoConicConformal,
+  geoConicEqualArea,
+  geoConicEquidistant,
+  geoAlbers,
+  geoAlbersUsa,
+  geoEquirectangular,
+  geoMercator,
+  geoTransverseMercator,
+  geoEqualEarth,
+  geoNaturalEarth1,
+} from "d3";
+
+// -- Azimuthal Projections
+export const geoAzimuthalEqualAreaPathGenerator = () => {
+  const projection = geoAzimuthalEqualArea();
+  return geoPath(projection);
+};
+
+export const geoAzimuthalEquidistantPathGenerator = () => {
+  const projection = geoAzimuthalEquidistant();
+  return geoPath(projection);
+};
+
+export const geoGnomonicPathGenerator = () => {
+  const projection = geoGnomonic();
+  return geoPath(projection);
+};
+
+export const geoOrthographicPathGenerator = () => {
+  const projection = geoOrthographic();
+  return geoPath(projection);
+};
+
+export const geoStereographicPathGenerator = () => {
+  const projection = geoStereographic();
+  return geoPath(projection);
+};
+
+// -- Conic Projections
+export const geoConicConformalPathGenerator = () => {
+  const projection = geoConicConformal();
+  return geoPath(projection);
+};
+
+export const geoConicEqualAreaPathGenerator = () => {
+  const projection = geoConicEqualArea();
+  return geoPath(projection);
+};
+
+export const geoConicEquidistantPathGenerator = () => {
+  const projection = geoConicEquidistant();
+  return geoPath(projection);
+};
+
+export const geoAlbersPathGenerator = () => {
+  const projection = geoAlbers();
+  return geoPath(projection);
+};
+
+export const geoAlbersUsaPathGenerator = () => {
+  const projection = geoAlbersUsa();
+  return geoPath(projection);
+};
+
+// -- Cylindrical Projections
+export const geoEquirectangularPathGenerator = () => {
+  const projection = geoEquirectangular();
+  return geoPath(projection);
+};
+
+export const geoMercatorPathGenerator = () => {
+  const projection = geoMercator();
+  return geoPath(projection);
+};
+
+export const geoTransverseMercatorPathGenerator = () => {
+  const projection = geoTransverseMercator();
+  return geoPath(projection);
+};
+
+export const geoEqualEarthPathGenerator = () => {
+  const projection = geoEqualEarth();
+  return geoPath(projection);
+};
+
+export const geoNaturalEarth1PathGenerator = () => {
+  const projection = geoNaturalEarth1();
+  return geoPath(projection);
+};
